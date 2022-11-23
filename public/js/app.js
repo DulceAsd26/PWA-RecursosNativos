3983317f0078ae88c46e7863c62923c390ca626a
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-11-22 22:59:39 -0600

    POST con las coordenadas y el mapa

--- public/js/app.js
+++ public/js/app.js
@@ -80,7 +80,7 @@ var usuario;
 
 // ===== Codigo de la aplicación
 
-function crearMensajeHTML(mensaje, personaje, lat, lng, foto) {
+function crearMensajeHTML(mensaje, personaje, lat, lng) {
 
     // console.log(mensaje, personaje, lat, lng);
 
@@ -99,12 +99,12 @@ function crearMensajeHTML(mensaje, personaje, lat, lng, foto) {
                 ${ mensaje }
                 `;
     
-    if ( foto ) {
-        content += `
-                <br>
-                <img class="foto-mensaje" src="${ foto }">
-        `;
-    }
+//    if ( foto ) {
+//        content += `
+//                <br>
+//                <img class="foto-mensaje" src="${ foto }">
+//        `;
+//    }
         
     content += `</div>        
                 <div class="arrow"></div>
@@ -245,7 +245,6 @@ postBtn.on('click', function() {
         user: usuario,
         lat: lat,
         lng: lng,
-        foto: foto
     };
 
 
@@ -260,10 +259,8 @@ postBtn.on('click', function() {
     .then( res => console.log( 'app.js', res ))
     .catch( err => console.log( 'app.js error:', err ));
 
-    camera.apagar();
-    contenedorCamara.addClass('oculto');
 
-    crearMensajeHTML( mensaje, usuario, lat, lng, foto );
+    crearMensajeHTML( mensaje, usuario, lat, lng );
     
     foto = null;
 });
@@ -277,6 +274,7 @@ function getMensajes() {
         .then( res => res.json() )
         .then( posts => {
 
+            console.log(posts);
 
             posts.forEach( post => 
                 crearMensajeHTML( post.mensaje, post.user, post.lat, post.lng, post.foto ));
@@ -493,6 +491,9 @@ btnLocation.on('click', () => {
 
         console.log( pos);
         mostrarMapaModal( pos.coords.latitude, pos.coords.longitude );
+
+        lat = pos.coords.latitude;
+        lng = pos.coords.longitude;
 
     });
     
--- server/routes.js
+++ server/routes.js
@@ -25,9 +25,14 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
   
 
+  console.log( req.body.lat );
+  console.log( req.body.lng );
+
   const mensaje = {
     mensaje: req.body.mensaje,
-    user: req.body.user
+    user: req.body.user,
+    lat: req.body.lat,
+    lng: req.body.lat
   };
 
   mensajes.push( mensaje );
